ecc0c9b7e9862017d1851cd1d6e96744d976ba7a
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-08-09 17:17:41 +0700

    upfile

---
+++ 6.5.0/aspnet-core/src/DemoProject.Core/DemoProjectConsts.cs
@@ -0,0 +1,20 @@
+﻿using DemoProject.Debugging;
+
+namespace DemoProject
+{
+    public class DemoProjectConsts
+    {
+        public const string LocalizationSourceName = "DemoProject";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "e8c32ee801a04e339465f38cf34a034c";
+    }
+}
---
+++ 6.5.0/aspnet-core/src/DemoProject.EntityFrameworkCore/EntityFrameworkCore/DemoProjectDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using DemoProject.Authorization.Roles;
+using DemoProject.Authorization.Users;
+using DemoProject.MultiTenancy;
+
+namespace DemoProject.EntityFrameworkCore
+{
+    public class DemoProjectDbContext : AbpZeroDbContext<Tenant, Role, User, DemoProjectDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public DemoProjectDbContext(DbContextOptions<DemoProjectDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
